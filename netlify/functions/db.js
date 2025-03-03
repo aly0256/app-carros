314b2598e20472d21448a7ec6e5cb44f97131b2f
Adrian Ortega <aosw82@gmail.com>
2025-03-03 15:39:22 -0700

    first comment

---
+++ netlify/functions/db.js
@@ -0,0 +1,19 @@
+exports.handler = async function(event, context) {
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',  // Permitir todos los orígenes, puedes especificar tu dominio si lo prefieres
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',  // Métodos permitidos
+      'Access-Control-Allow-Headers': 'Content-Type',  // Cabeceras permitidas
+    },
+    body: JSON.stringify({
+      carros: [
+        { id: '1', marca: 'Chevrolet', modelo: 'Camaro', ano: '2015', precio: '45000'},
+        { id: '2', marca: 'Ford', modelo: 'Mustang', ano: '2011', precio: '115000'},
+        { id: '3', marca: 'Pontiac', modelo: 'Firebird', ano: '2018', precio: '90000'},
+        { id: '4', marca: 'Grand Cheroke', modelo: 'Jeep', ano: '2024', precio: '150000'},
+        { id: '5', marca: 'Grand Marquiz', modelo: 'Crown Victoria', ano: '1995', precio: '14000'}
+      ]
+    }),
+  };
+};
