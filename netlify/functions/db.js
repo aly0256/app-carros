09d85658db59ac0e7325ee28a73544a73b0ac0ab
Adrian Ortega <aosw82@gmail.com>
2025-03-03 15:55:21 -0700

    new cambio

--- netlify/functions/db.js
+++ netlify/functions/db.js
@@ -12,7 +12,9 @@ exports.handler = async function(event, context) {
         { id: '2', marca: 'Ford', modelo: 'Mustang', ano: '2011', precio: '115000'},
         { id: '3', marca: 'Pontiac', modelo: 'Firebird', ano: '2018', precio: '90000'},
         { id: '4', marca: 'Grand Cheroke', modelo: 'Jeep', ano: '2024', precio: '150000'},
-        { id: '5', marca: 'Grand Marquiz', modelo: 'Crown Victoria', ano: '1995', precio: '14000'}
+        { id: '5', marca: 'Grand Marquiz', modelo: 'Crown Victoria', ano: '1995', precio: '14000'},
+        { id: '6', marca: 'Nuevo carro', modelo: 'Crown Victoria', ano: '1995', precio: '14000'},
+        { id: '7', marca: 'Chrysler', modelo: '300ZR', ano: '2024', precio: '850000'}
       ]
     }),
   };
